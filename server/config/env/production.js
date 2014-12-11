a6cef63c84bc74f93d9cd8bf512942265e7b5bb3
Jesse Himmelstein <jesse@heavenstone.net>
2014-12-11 11:01:27 +0100

    Removed github login (not working)

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -3,10 +3,6 @@
 module.exports = {
 	db: 'mongodb://localhost/ideaweave',
     crossOrigin : 'http://ideastorm.io',
-    TOKEN_SECRET: 'MyWonderfullCridevlesinternetsmondiaux',
-    GITHUB_SECRET: 'a9c4e876e84a60db81e2bceb449fc7b848e7b756',
-    GOOGLE_SECRET: '51P8sMOw1ZgYen7dOe7orHHv',
-    TWITTER_KEY: 'Twitter Consumer Key',
-    TWITTER_SECRET : 'a9c4e876e84a60db81e2bceb449fc7b848e7b756',
-    TWITTER_CALLBACK: 'localhost:5000/home'
+    TOKEN_SECRET: 'mysecret',
+    GOOGLE_SECRET: ''
 };
