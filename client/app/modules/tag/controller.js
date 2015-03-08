f101c5c49018f9e451b7edfaeb4403aef5a40895
Jesse Himmelstein <jesse@heavenstone.net>
2015-03-08 21:43:38 +0100

    Tag list styles

--- client/app/modules/tag/controller.js
+++ client/app/modules/tag/controller.js
@@ -1,6 +1,7 @@
 angular.module('cri.tag',[])
     .controller('TagCtrl',['$scope','$state','$stateParams','Tag','$timeout','$sce','Notification',function($scope,$state,$stateParams,Tag,$timeout,$sce,Notification){
     	$scope.isLoading = true;
+    	$scope.tag = $stateParams.title;
 
         Tag.search($stateParams.title).then(function(data){
             $scope.tagData=data;
