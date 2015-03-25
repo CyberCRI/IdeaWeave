d470d2478a1d7e1f05d9b0d9c88a73f99f7267ad
Judith Boldt <judithboldt@yahoo.com>
2015-03-25 19:35:22 +0100

    routes for idea

---
+++ server/app/routes/idea.routes.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * Module dependencies
+ */
+var idea = require('../controllers/idea.controller.js'),
+	utils = require('../services/utils.service');
+
+module.exports = function(app) {
+	app.route('/ideas')
+		.get(idea.fetch)
+		.post(utils.ensureAuthenticated, idea.create);
+	app.route('/idea/:id')
+		.delete(utils.ensureAuthenticated, idea.remove);
+};
