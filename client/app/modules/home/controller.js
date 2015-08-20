20fbf5b57ae3dd32737e14c1a907b69ec5ece8ba
Bari Mourdjen <liaroc13@gmail.com>
2015-08-20 11:11:20 +0200

    max limit of brief in home page project card

--- client/app/modules/home/controller.js
+++ client/app/modules/home/controller.js
@@ -1,11 +1,19 @@
 angular.module('cri.home',[])
 .controller('HomeCtrl',['$scope','$state','popularThings','tags','$location','$anchorScroll',
     function($scope,$state,popularThings,tags,$location, $anchorScroll){
+        var maximumCharBrief = 30;
         $scope.challenges = popularThings.data.challenges || [];
         $scope.challenges.unshift({intro : 3});
         $scope.challenges.unshift({intro : 2});
         $scope.challenges.unshift({intro : 1});
         $scope.projects = popularThings.data.projects || [];
+
+
+        for(var i = $scope.projects.length;i--;){
+            if($scope.projects[i].brief.length > maximumCharBrief) {
+                $scope.projects[i].brief = $scope.projects[i].brief.slice(0, maximumCharBrief - 4) + " ...";
+            }
+        }
 
         $scope.tags = tags;
 
