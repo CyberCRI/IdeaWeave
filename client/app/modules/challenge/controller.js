53e071d866f978b69aed98431d73febef96d96d8
Bari Mourdjen <liaroc13@gmail.com>
2015-09-16 09:58:18 +0200

    try to add new date picker with min date, but not working

--- client/app/modules/challenge/controller.js
+++ client/app/modules/challenge/controller.js
@@ -64,7 +64,10 @@ angular.module('cri.challenge', ['ngSanitize'])
         $scope.hasDuration = false;
         $scope.pform = {};
         $scope.pform.tags = [];
+        $scope.pform.startDate = new Date();
+        $scope.pform.endDate = new Date();
         $scope.tinymceOption = Config.tinymceOptions;
+        // $scope.pform.startDate = 
         $scope.refreshAddresses = function(address) {
             Gmap.getAdress(address).then(function(adresses){
                 $scope.addresses = adresses;
