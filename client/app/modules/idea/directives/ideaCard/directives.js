714fb464badf8229a20a131f86c5800f7e220770
Jesse Himmelstein <jesse@heavenstone.net>
2015-12-10 10:37:31 +0100

    Fixed like and dislike buttons on idea cards

--- client/app/modules/idea/directives/ideaCard/directives.js
+++ client/app/modules/idea/directives/ideaCard/directives.js
@@ -16,16 +16,19 @@ angular.module('cri.idea')
             } else {
                 Idea.fetch($scope.ideaId).then(function(idea){
                     $scope.idea = idea;
-                    $scope.idea.brief = cutText($scope.idea.brief,90);
                 }).catch(function(err){
                     console.log('error',err);
                 });
             }
 
+            $scope.currentUser = $rootScope.currentUser;
+
             var updateIdea = function() {
-                console.log($scope);
+                if(!$scope.idea) return;
+
+                $scope.idea.brief = cutText($scope.idea.brief, 90);
                 $scope.isFollow = $scope.currentUser ?  _.chain($scope.idea.followers).pluck("_id").contains($scope.currentUser._id).value() : false;
-                $scope.isLike = $scope.currentUser ?  _.contains($scope.idea.likers,$scope.currentUser._id) : false;
+                $scope.isLike = $scope.currentUser ?  _.contains($scope.idea.likerIds,$scope.currentUser._id) : false;
             };
 
             $scope.$watch('idea',updateIdea);
@@ -41,13 +44,8 @@ angular.module('cri.idea')
             };
 
             $scope.follow=function(){
-                var param;
                 if($scope.isFollow){
-                    param = {
-                        follower : $scope.currentUser._id,
-                        following : $scope.idea._id
-                    };
-                    Idea.unfollow(param).then(function(result){
+                    Idea.unfollow($scope.idea._id).then(function(result){
                         Notification.display('You will no longer be notified about this idea');
                         $scope.idea.followers.splice($scope.idea.followers.indexOf($scope.currentUser._id),1);
                         $scope.isFollow=false;
@@ -56,11 +54,7 @@ angular.module('cri.idea')
                         Notification.display(err.message);
                     });
                 }else{
-                    param = {
-                        follower : $scope.currentUser._id,
-                        following : $scope.idea._id
-                    };
-                    Idea.follow(param).then(function(result){
+                    Idea.follow($scope.idea._id).then(function(result){
                         Notification.display('You will now be notified about this idea');
                         $scope.idea.followers.push($scope.currentUser._id);
                         $scope.isFollow=true;
@@ -75,7 +69,7 @@ angular.module('cri.idea')
                 if($scope.isLike){
                     Idea.dislike($scope.idea._id).then(function(result){
                         Notification.display('You no longer like this idea');
-                        $scope.idea.likers.splice($scope.idea.likers.indexOf($scope.currentUser._id),1);
+                        $scope.idea.likerIds.splice($scope.idea.likerIds.indexOf($scope.currentUser._id),1);
                         $scope.isLike=false;
                         $analytics.eventTrack("dislikeIdea");
                     }).catch(function(err){
@@ -84,7 +78,7 @@ angular.module('cri.idea')
                 }else{
                     Idea.like($scope.idea._id).then(function(result){
                         Notification.display('You like this idea');
-                        $scope.idea.likers.push($scope.currentUser._id);
+                        $scope.idea.likerIds.push($scope.currentUser._id);
                         $scope.isLike=true;
                         $analytics.eventTrack("likeIdea");
                     }).catch(function(err){
